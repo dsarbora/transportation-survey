91f97f34e9b9f5c101b2d7ce7f9c04401551f90d
Dave Sarbora and Tristan Setha <dave@>
2019-01-23 14:48:28 -0800

    made the whole survey

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+  $("#transportation_survey").submit(function(){
+    debugger;
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $("#work-responses").append(workTransportationMode + "<br>");
+    }); //end input .each function
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $("#transportation_survey").hide();
+  });  //end form submit function
+});  //end document ready
